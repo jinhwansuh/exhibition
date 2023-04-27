2cac047fec4faee0cd312c487efcf0f7a840a4e1
jinhwansuh <nick02190219@gmail.com>
2023-04-28 03:36:37 +0900

    fix: 물체가 +방향으로 움직이도록 변경

--- src/components/Ball.tsx
+++ src/components/Ball.tsx
@@ -7,30 +7,30 @@ function Ball({ floor }) {
   const ref = useRef<Mesh>(null!);
   const keyMap = useKeyboard();
 
-  const v = useMemo(() => new Vector3(), []);
-  const q = useMemo(() => new Quaternion(), []);
-  const angularVelocity = useMemo(() => new Vector3(), []);
+  // const v = useMemo(() => new Vector3(), []);
+  // const q = useMemo(() => new Quaternion(), []);
+  // const angularVelocity = useMemo(() => new Vector3(), []);
 
   useFrame((_, delta) => {
-    keyMap['KeyW'] && (angularVelocity.x -= delta * 5);
-    keyMap['KeyS'] && (angularVelocity.x += delta * 5);
-    keyMap['KeyA'] && (angularVelocity.z += delta * 5);
-    keyMap['KeyD'] && (angularVelocity.z -= delta * 5);
+    keyMap['KeyW'] && (ref.current.position.x += delta * 5);
+    keyMap['KeyS'] && (ref.current.position.x -= delta * 5);
+    keyMap['KeyA'] && (ref.current.position.z -= delta * 5);
+    keyMap['KeyD'] && (ref.current.position.z += delta * 5);
 
-    q.setFromAxisAngle(angularVelocity, delta).normalize();
-    ref.current.applyQuaternion(q);
+    // q.setFromAxisAngle(angularVelocity, delta).normalize();
+    // ref.current.applyQuaternion(q);
 
-    angularVelocity.lerp(v, 0.01); // slow down the roll
+    // angularVelocity.lerp(v, 0.01); // slow down the roll
 
-    floor.current.position.x += angularVelocity.z * delta;
-    floor.current.position.z -= angularVelocity.x * delta;
+    // floor.current.position.x += angularVelocity.z * delta;
+    // floor.current.position.z -= angularVelocity.x * delta;
 
-    floor.current.position.x = floor.current.position.x % 10;
-    floor.current.position.z = floor.current.position.z % 10;
+    // floor.current.position.x = floor.current.position.x % 10;
+    // floor.current.position.z = floor.current.position.z % 10;
   });
 
   return (
-    <mesh ref={ref} position-y={1.0}>
+    <mesh ref={ref} position-y={0.3}>
       <sphereGeometry />
       <meshNormalMaterial wireframe />
     </mesh>
